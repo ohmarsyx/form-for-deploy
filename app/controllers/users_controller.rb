269e33ced0750238bc931692ba4dc8f25228d5f0
ohmarsyx <ohmarsy@odds.team>
2024-09-04 12:31:42 +0700

    feat: register form no style with function post create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @user = User.all
+    @users = User.all
   end
 
   def show
@@ -17,16 +17,22 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: "User was successfully created." }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      redirect_to root_path
+    else
+      format.html { render :new, status: :unprocessable_entity }
     end
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
 
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :birthday, :gender, :email, :phone, :subject)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ApplicationRecord
+  enum gender: [:Male, :Female]
 end
